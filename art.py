3fe751c22b08dd0c9f9764a2ac9fc77f9b40c107
kaal <kaalharper@protonmail.com>
2022-04-27 19:52:10 +0530

    restruct

--- art.py
+++ art.py
@@ -29,75 +29,60 @@ class common():
     features,
     features_plot,
     data_process"""
-    def __init__(self,df,data_prepare=None,target='target',mtype='simple',method=None,metric='mse',**kwargs):
-        print("\n\n=>Attributes  [df,data_prepare=None,target='target',mtype='simple',method='zscore',metric='mse'] \n\n=>Methods \n1.features() \n2.feature_plot(ptype='corelation') \n3.data=data_process()  \n4.train(data=data,model=model)\n\n")
+    def __init__(self,df,target='target',ptype='clf',mtype='simple',method=None,metric='mse',**kwargs):
+        print("\n\n=>Attributes  [df,target='target',ptype='clf',mtype='simple',method='zscore',metric='mse'] \n\n=>Methods \n1.features() \n2.feature_plot(ptype='corelation') \n3.data=data_process()  \n4.train(data=data,model=model)\n\n")
         self.df=df
-        self.data_prepare=data_prepare
         self.target=target
         self.mtype=mtype
         self.method=method
         self.metric=metric
-    def features(self,num_col=False,obj_col=False,dt_col=False):
+        self.ptype=ptype
+    def features(self,more_col=False):
         """Basic overviews of features
         for specific features : num_col,obj_col,dt_col"""
         ddf=self.df
-        df1=pd.DataFrame()
+        features_df=pd.DataFrame()
         self.features=np.array(ddf.columns.tolist())
-        if num_col :
+        if more_col :
             s = (self.df.dtypes != 'object'  )
             self.features = np.array(s[s].index)
-            df1['skewness']=list(map(lambda x : self.df[x].skew(),self.features))
-            df1['kurtos']=list(map(lambda x : self.df[x].kurt(),self.features))
-        if obj_col :
-            s = (self.df.dtypes == 'object' ) 
-            self.features = np.array(s[s].index) 
-        if dt_col :
-            s = (self.df.dtypes == 'datetime64' ) 
-            self.features = np.array(s[s].index) 
-        df1['features']=self.features  
-        df1['dtype']=list(map(lambda x : self.df[x].dtype,self.features))
-        df1['count']=list(map(lambda x : self.df[x].count(),self.features))
-        df1['null_values']=list(map(lambda x :self.df[x].isnull().sum(),self.features))
-        df1['uniques']=list(map(lambda x :len(self.df[x].unique()),self.features))
-        df1['values']=list(map(lambda x : self.df[x].unique() if len(self.df[x].unique()) < 10 else self.df[x].values[:4].tolist(), self.features))
-        df1['value_counts']=list(map(lambda x : self.df[x].value_counts().values  if len(self.df[x].unique()) < 10 else self.df[x].value_counts().values[:4].tolist(),self.features ))
-        df1.set_index('features',inplace=True)
-        return df1
+            features_df['skewness']=list(map(lambda x : self.df[x].skew(),self.features))
+            features_df['kurtos']=list(map(lambda x : self.df[x].kurt(),self.features))
+
+        features_df['features']=self.features  
+        features_df['dtype']=list(map(lambda x : self.df[x].dtype,self.features))
+        features_df['count']=list(map(lambda x : self.df[x].count(),self.features))
+        features_df['null_values']=list(map(lambda x :self.df[x].isnull().sum(),self.features))
+        features_df['uniques']=list(map(lambda x :len(self.df[x].unique()),self.features))
+        features_df['values']=list(map(lambda x : self.df[x].unique() if len(self.df[x].unique()) < 10 else self.df[x].values[:4].tolist(), self.features))
+        features_df['value_counts']=list(map(lambda x : self.df[x].value_counts().values  if len(self.df[x].unique()) < 10 else self.df[x].value_counts().values[:4].tolist(),self.features ))
+        features_df.set_index('features',inplace=True)
+        return features_df
 
     def features_plot(self,ptype='basic'):
         """ptype : default plot : basic || 
             plots : corelation ,cmatrix,"""
         fig=plt.figure(figsize=(16,9))
         ax=fig.gca()
-        
         if ptype=='basic' :    
             self.df.hist(ax=ax,bins=10,)
-
         if ptype=='corelation' :
             sns.heatmap(self.df.corr(),ax=ax,cmap='Dark2')
-
         plt.show()
 
-    def numcol_prepare(self,df):
-        """Basic preparation of data :
-        removing object features
-        removing nan values"""
-        df.drop((df.dtypes=='object')[df.dtypes =='object'].index.tolist(),axis=1,inplace=True)
-        df.dropna(axis=0,inplace=True)
-        return df
+   
     
-    def data_process(self):
+    def data_process(self,data_prepare=None):
         """
         To process the data to feed model
         func = your function that process raw data to universel data
         target = default : 'target' ,you can provide your target value
         """
-        
-        if self.data_prepare!=None :
+        if data_prepare!=None :
             self.df=self.data_prepare(self.df)
         else :
-            self.df=self.numcol_prepare(self.df)
-            
+            self.df.drop((self.df.dtypes=='object')[self.df.dtypes =='object'].index.tolist(),axis=1,inplace=True)
+            self.df=self.df.dropna(axis=0,inplace=True)   
         X=self.df.drop(self.target,axis=1)
         y=self.df[self.target]
 
@@ -106,16 +91,44 @@ class common():
             nm=methods[self.method]
             print(nm)
             X=nm.fit_transform(np.array(X))
-
         if self.mtype=='deep' :
             y=to_categorical(y,len(self.df[self.target].unique()))
-     
+        return X,y
         
-        train_X,val_X,train_y,val_y=train_test_split(X,y,test_size=0.2,random_state=42)
-        print(f"train set : {train_X.shape,train_y.shape} val set : {val_X.shape,val_y.shape}")
-        return train_X,val_X,train_y,val_y
+    def validate(self,X,y,type='holdout'):
+        if type=='holdout':
+            train_X,val_X,train_y,val_y=train_test_split(X,y,test_size=0.2,random_state=42)
+            print(f"train set : {train_X.shape,train_y.shape} val set : {val_X.shape,val_y.shape}")
+            return train_X,val_X,train_y,val_y
+        if type=='kfold':
+            pass
+
+
+    def fitting(self,train_X,train_y):
+        if self.mtype=='simple':
+            model.fit(train_X,train_y) 
+        if self.mtype=='deep':
+            model.fit(train_X,train_y,epochs=1,batch_size=32,verbose=0)         
     
+    def predicting(self,train_X):
+        val_yhat=model.predict(val_X)
+        train_yhat=model.predict(train_X)
     
+    def scoring(self,actual,yhat):
+        if self.ptype=='clf':
+            if self.mtype=='simple':
+                train_score=accuracy_score(actual,yhat)
+            if self.mtype=='deep':
+                train_score=accuracy_score(np.argmax(actual,axis=1),np.argmax(yhat,axis=1))
+        if self.ptype=='reg':
+            metrics={'mse':mean_squared_error,'mae':mean_absolute_error}
+            score_=metrics[self.metric]
+            if self.mtype=='simple':    
+                train_score=score_(actual,yhat)
+            if self.mtype=='deep':
+                train_score=score_(actual,np.argmax(yhat,axis=1))
+        print(f" score for {actual} : {train_score} ")
+
     
     
     
@@ -124,33 +137,25 @@ class common():
 
 class classification(common):
     def __init__(self, df, **kwargs):
+        print('what"s your class ?')
         super().__init__(df, **kwargs)
-    
-    
-    
-    
-    
+       
     def train(self,data,model=RandomForestClassifier()):
         """training basic model
         model=your custom model function
         mtype= defalut: simple
         simple : sklearn model
         deep : neural model"""
-        train_X,val_X,train_y,val_y=data
-        if self.mtype=='simple':
-            model.fit(train_X,train_y) 
-        if self.mtype=='deep':
-            model.fit(train_X,train_y,validation_data=(val_X,val_y),epochs=1,batch_size=32,verbose=0)        
+        X,y=self.data_process()
+        train_X,val_X,train_y,val_y=self.validate()
+        self.fitting()
+        self.predicting()
+              
         val_yhat=model.predict(val_X)
         train_yhat=model.predict(train_X)
-        if self.mtype=='simple':
-            train_score=accuracy_score(train_y,train_yhat)
-            val_score=accuracy_score(val_y,val_yhat)
-        if self.mtype=='deep':
-            train_score=accuracy_score(np.argmax(train_y,axis=1),np.argmax(train_yhat,axis=1))
-            val_score=accuracy_score(np.argmax(val_y,axis=1),np.argmax(val_yhat,axis=1))
-            return print(f"train score : {train_score} val score : {val_score}")
-        return print(f"train score : {train_score} val score : {val_score}")
+        self.scoring(train_y,train_yhat)
+        self.scoring(val_y,val_yhat)
+        
         
 class regression(common):
     def __init__(self, df, **kwargs):
@@ -172,20 +177,13 @@ class regression(common):
             model.fit(train_X,train_y,validation_data=(val_X,val_y),epochs=1,batch_size=32,verbose=0)        
         val_yhat=model.predict(val_X)
         train_yhat=model.predict(train_X)
-        metrics={'mse':mean_squared_error,'mae':mean_absolute_error}
-        score_=metrics[self.metric]
-        if self.mtype=='simple':
-            train_score=score_(train_y,train_yhat)
-            val_score=score_(val_y,val_yhat)
-        if self.mtype=='deep':
-            train_score=score_(np.argmax(train_y,axis=1),np.argmax(train_yhat,axis=1))
-            val_score=score_(np.argmax(val_y,axis=1),np.argmax(val_yhat,axis=1))
-            return print(f"train {self.metric} : {train_score} val {self.metric} : {val_score}")
-        return print(f"train {self.metric} : {train_score} val {self.metric} : {val_score}")
+        
+        
 
 
 class timeseries(common):
     def __init__(self,df,**kwargs):
+        print('time is yours')
         super().__init__(df,**kwargs)
          
     def data_process(self):
@@ -225,13 +223,13 @@ class timeseries(common):
 
 
 class auto(classification,regression):
-    def __init__(self,df,ptype='clf',model=RandomForestClassifier(),**kwargs):
-        print("Extra Attributes : ptype :['clf','reg']")
+    def __init__(self,df,model=RandomForestClassifier(),**kwargs):
+        print("Extra Attributes : ptype :['clf','reg','timeseries']")
         super().__init__(df ,**kwargs)
         problem={'clf':classification ,'reg': regression}
         
         data=self.data_process()
-        problem[ptype].train(self,data=data,model=model)
+        problem[self.ptype].train(self,data=data,model=model)
     
 
 
