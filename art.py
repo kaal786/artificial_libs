083ea58f8ec005c3a3b8a71e12e2b96303304c7a
kaal <kaalharper@protonmail.com>
2022-09-28 01:13:08 +0530

    error in reg

--- art.py
+++ art.py
@@ -1,11 +1,15 @@
 # external libs for use 
 from abc import abstractmethod
+from logging import exception
 from turtle import shape
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
+plt.rcParams['figure.figsize']=(12,6)
+plt.style.use('fivethirtyeight')
+
 from math import ceil
 import random 
 
@@ -23,8 +27,7 @@ from sklearn.linear_model import LinearRegression
 
 
 import tensorflow as tf
-from tensorflow import random
-from tensorflow.keras.utils.np_utils import to_categorical
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input, Dense ,Dropout
 from tensorflow.keras.models import Model
 
@@ -33,9 +36,8 @@ from tensorflow.keras.models import Model
 #set seeds
 random.seed(42)
 np.random.seed(42)
-random.set_seed(42)
+tf.random.set_seed(42)
 torch.manual_seed(42)
-
 
 
 
@@ -63,9 +65,11 @@ class art():
         self.metric=metric
         self.ptype=ptype
         self.validation=validation
+       
+
     def features(self,more_col=False):
         """Basic overviews of features
-        for specific features : num_col,obj_col,dt_col"""
+            for specific features : num_col,obj_col,dt_col"""
         ddf=self.df
         features_df=pd.DataFrame()
         self.features=np.array(ddf.columns.tolist())
@@ -88,8 +92,11 @@ class art():
     def features_plot(self,plotype='basic'):
         """ptype : default plot : basic || 
             plots : corelation ,cmatrix,features"""
-        if plotype=='basic' :    
-            self.df.hist(self.df,bins=10,)
+        if plotype=='basic' : 
+            try:   
+                self.df.hist()
+            except exception as e:
+                print(e)
         if plotype=='corelation' :
             sns.heatmap(self.df.corr(),cmap='Dark2')
         if plotype=='features':
@@ -108,6 +115,7 @@ class art():
         To process the data to feed model
         func = your function that process raw data to universel data
         target = default : 'target' ,you can provide your target value
+        return features , label
         """
         if data_prepare!=None :
             self.df=data_prepare(self.df)
@@ -121,24 +129,25 @@ class art():
             nm=methods[self.method]
             print(nm)
             X=nm.fit_transform(np.array(X))
-        if self.mtype=='deep' :
+        if self.mtype=='deep' & self.ptype=='clf':
             y=to_categorical(y,len(self.df[self.target].unique()))
-        return X,y
+        return self.X,self.y
         
     def validate(self,X,y,type='holdout'):
         if type=='holdout':
             train_X,val_X,train_y,val_y=train_test_split(X,y,test_size=0.2,random_state=42)
             print(f"train set : {train_X.shape,train_y.shape} val set : {val_X.shape,val_y.shape}")
-            return train_X,val_X,train_y,val_y
+            return train_X,train_y,val_X,val_y
         if type=='kfold':
             pass
-
+        if type=='groupKfold':
+            pass
 
     def fitting(self,model,train_X,train_y):
         if self.mtype=='simple':
             model.fit(train_X,train_y) 
         if self.mtype=='deep':
-            model.fit(train_X,train_y,epochs=1,batch_size=32,verbose=0)         
+            self.model.fit(train_X,train_y,epochs=1,batch_size=32,verbose=0)         
 
     def scoring(self,actual,yhat):
         if self.ptype=='clf':
@@ -155,64 +164,75 @@ class art():
             if self.mtype=='deep':
                 train_score=score_(actual,np.argmax(yhat,axis=1))
             print(f"  {self.metric} score : {train_score} ")
-
-    
-    def train(self,data_prepare=None):
+   
+    def train(self,model=None,data_prepare=None):
         """training basic model
         model=your custom model function
         mtype= defalut: simple
         simple : sklearn model
         deep : neural model"""
-        models={'clf': RandomForestClassifier,'reg': RandomForestRegressor ,'timeseries' :LinearRegression}
-        model=models[self.ptype]()
-        X,y=self.data_process(data_prepare=data_prepare)
-        if self.validation and self.ptype!='timeseries' :
-            X,val_X,y,val_y=self.validate(X,y)
-            self.fitting(model,X,y)
-            val_yhat=model.predict(val_X)
-            self.scoring(val_y,val_yhat)
-        else :      
-            self.fitting(model,X,y)
-        train_yhat=model.predict(X)
-        self.scoring(y,train_yhat)
+       
 
+        self.model.fit(self.X,self.y)
+        if self.validation and self.ptype!='timeseries' :
+            val_yhat=self.model.predict(val_X)
+            # self.scoring(val_y,val_yhat)
+     
+        train_yhat=self.model.predict(X)
+        # self.scoring(y,train_yhat)
         
-    def network_(self,input_shape,output_shape):
+    def network_(self,input_sizequery,output_shape):
         """deep model networks
-        input_shape=tuple e.g (30,)
+        input_sizequery=tuple e.g (30,)
         ouput_shape=no of output 
 
 
         """
-        input=Input(shape=input_shape)
+        input=Input(shape=input_sizequery)
         x=Dense(units=256,activation='relu')(input)
         x=Dense(units=128,activation='relu')(x)
         x=Dense(units=64,activation='relu')(x)
         output=Dense(units=output_shape)(x)
+        model=Model(inputs=input,outputs=output)
         if self.ptype=='clf':
-            self.model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+            model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
         if self.ptype=='reg':
-            self.model.compile(optimizer='adam',loss='mae',metrics='mse')
-        self.model=Model(inputs=input,outputs=output)
-        print(self.model.summary())
-        return self.model
+            model.compile(optimizer='adam',loss='mae',metrics='mse')
+        print(model.summary())
+        return model
     
 
-
-class classification(art):
+class clf(art):
     def __init__(self, df, **kwargs):
         print('what"s your class ?')
         super().__init__(df, **kwargs)
         self.ptype='clf'
     
-
-class regression(art):
-    def __init__(self, df, **kwargs):
+class reg(art):
+    def __init__(self, df,model=None, **kwargs):
         super().__init__(df,**kwargs)
         self.ptype='reg'
+        X,y=self.data_process()
+        # X,y,val_X,val_y=self.validate(X,y)
+        # input_sizequery=self.X.shape[1:]
+        # output_shape=self.y.shape
+        # print(X.shape,y.shape)
+        # self.models={'clf':{'simple': RandomForestClassifier(),
+        #                 'deep':self.network_(input_sizequery=input_sizequery,output_shape=output_shape)},
+        #             'reg':{'simple':RandomForestRegressor(),
+        #                 'deep' :self.network_(input_sizequery=input_sizequery,output_shape=output_shape)},
+        #             'timeseries':{'simple':LinearRegression(),}
+        #             }
+        # if model==None :
+        #     self.model=self.models[self.ptype][self.mtype]
+        # else:
+        #     self.model=model
+        
+        # self.train()
+
+
+        
     
-
-
 class timeseries(art):
     def __init__(self,df,**kwargs):
         print('time is yours')
@@ -220,19 +240,15 @@ class timeseries(art):
         self.ptype='timeseries'
 
 
-
-
-
-class auto(classification,regression):
-    def __init__(self,df,model=RandomForestClassifier(),**kwargs):
-        print("Extra Attributes : ptype :['clf','reg','timeseries']")
-        super().__init__(df ,**kwargs)
-        problem={'clf':classification ,'reg': regression}
+# class auto(classification,regression):
+#     def __init__(self,df,model=RandomForestClassifier(),**kwargs):
+#         print("Extra Attributes : ptype :['clf','reg','timeseries']")
+#         super().__init__(df ,**kwargs)
+#         problem={'clf':classification ,'reg': regression}
         
-        data=self.data_process()
-        problem[self.ptype].train(self,data=data,model=model)
+#         data=self.data_process()
+#         problem[self.ptype].train(self,data=data,model=model)
     
-
 
 if __name__=='__main__':
     print("Hello")
